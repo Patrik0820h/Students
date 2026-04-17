8ccf3341f879ea71fd85044e7edd53722180897f
Molnár Patrik Márk 446 <molpat446@logiker.hu>
2026-04-17 13:30:12 +0200

    student list

--- Client/students.js
+++ Client/students.js
@@ -1,13 +1,28 @@
 fetch('/liststudents')
 .then(function (res) {return res.json()})
 .then(function (data) {
-    let students = document.getElementById('students');
+    let studentsBlock = document.getElementById('students');
     let studentClass = "";
-    data.foreach(function (student){
-        if (studentClass != student.osztaly); 
-        students.innerHTML += "<table id='"+studentClass+"'>"
-
+    let studentTable;
+  
+    data.forEach(function (student){
+        
+        if (studentClass != student.osztaly){
+            if (studentClass != ""){
+                studentsBlock.appendChild(studentTable);
+            }
+            studentClass = student.osztaly;
+            studentTable = document.createElement("table");
+            studentTable.id = studentClass;
+        };
+        let sor = document.createElement("tr");
+        let cella = document.createElement("td");
+        cella.innerHTML = student.id;
+        let cella2 = document.createElement("td");
+        cella2.innerHTML = student.nev;
+        sor.appendChild(cella);
+        sor.appendChild(cella2);
+        studentTable.appendChild(sor);
     })
-      
-    .innerHTML = data;
+    studentsBlock.appendChild(studentTable);
 })
